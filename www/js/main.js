e702693ab31a6e5a64e6f5f9a219bb0a4c30a6de
jowen <divers@ix.netcom.com>
2019-12-13 14:52:30 -0800

    tt

--- www/js/main.js
+++ www/js/main.js
@@ -1259,15 +1259,13 @@ function FollowMyPhone(myState)
                 break;
             }
             
-            followState++;
-            if( followState >= FOLLOW_STATE_DONE )
-            {
-                setTimeout( function(){ FollowMyPhone(FOLLOW_STATE_WORK); }, 1000 );  // Come back in 1 second
-            }
-            
         }
-        
-        
+
+        followState++;
+        if( followState >= FOLLOW_STATE_DONE )
+        {
+            setTimeout( function(){ FollowMyPhone(FOLLOW_STATE_WORK); }, 1000 );  // Come back in 1 second
+        }
 
     }
     else
