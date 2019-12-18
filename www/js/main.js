5761c30decd47b260522a988e7cb145ee7ad0737
jowen <divers@ix.netcom.com>
2019-12-18 14:13:03 -0800

    t

--- www/js/main.js
+++ www/js/main.js
@@ -916,7 +916,6 @@ var app = {
 //        var myBluetoothIcon = isSouthBoundIfCnx ? "<div id='bt_icon_id' class='bt_icon'>" + szSbIfIconOn + "</div>" : "<div  id='bt_icon_id' class='bt_icon'>" + szSbIfIconOff + "</div>";
 //        var myBluetoothMain = isSouthBoundIfCnx ? "<div id='bt_main_id' class='bt_main_icon'>" + szSbIfMainOn + "</div>" : "<div  id='bt_main_id' class='bt_main_icon'>" + szSbIfMainOff + "</div>";
         
-        if(bWaveTest)
         {
             var myHtml = 
 
