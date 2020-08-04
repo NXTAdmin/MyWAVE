91f2615047582f211dc16ca9c724026d3ee033b7
jowen <divers@ix.netcom.com>
2020-08-04 11:53:43 -0700

    test

--- www/js/main.js
+++ www/js/main.js
@@ -254,6 +254,7 @@ var app = {
                     "</label>" +
                 "</div>" +
                 "<div class='div_footer'>" +
+                    "<div class='test_version'>Xarfcn</div>" +
                     "<div class='text_version'>Version 1.0.1</div>" +
                     "<div class='div_footeroption'><a href='https://cel-fi.com/support/'><img src='img/HelpIcon.svg' alt='' class='image_footericon'></a></div>" +
                 "</div>" +
