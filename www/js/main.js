93178864b781bc72ee06c301e5634d15f5a4f9d8
jowen <divers@ix.netcom.com>
2020-09-11 08:24:15 -0700

    01.01.05

--- www/js/main.js
+++ www/js/main.js
@@ -59,7 +59,7 @@ var bNaking                 = false;
 var isNetworkConnected      = null;
 var bGotUserInfoRspFromCloud    = false;
 var msgTimer                = null; 
-var szVersion               = "00.01.05";
+var szVersion               = "01.01.05";
 
 
 var szSuccess               = "";
